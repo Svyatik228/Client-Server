65d0e0e14f1e04678a3b9c32d35bd784faf98641
Святослав Волошко <voloshko.svyatoslav2@gmail.com>
2020-03-17 23:06:07 +0200

    Add project files.

---
+++ Client_Server(TCP)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            // 1) IPv4  2)для tcp-Stream  3)Protocol
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Enter Message");
+            var message = Console.ReadLine();
+            var data = Encoding.UTF8.GetBytes(message);
+            tcpSocket.Connect(tcpEndPoint);
+            tcpSocket.Send(data);
+            var buffer = new byte[256];
+            var size = 0;
+            var answer = new StringBuilder();
+            do
+            {
+                size = tcpSocket.Receive(buffer);
+                answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+            }
+            while (tcpSocket.Available > 0);
+            Console.WriteLine(answer.ToString());
+            tcpSocket.Shutdown(SocketShutdown.Both);
+            tcpSocket.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Trying
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            // 1) IPv4  2)для tcp-Stream  3)Protocol
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //зв'язування з портом
+            tcpSocket.Bind(tcpEndPoint);
+            //режим прослуховування 
+            tcpSocket.Listen(10);
+            while (true)
+            {
+                var listener = tcpSocket.Accept();
+                var buffer = new byte[256];
+                var size = 0;
+                var data = new StringBuilder();
+                do
+                {
+                    size = listener.Receive(buffer);
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                }
+                while (listener.Available > 0);
+                Console.WriteLine(data);
+                listener.Send(Encoding.UTF8.GetBytes("Успех"));
+                listener.Shutdown(SocketShutdown.Both);
+                listener.Close();
+            }
+        }
+    }
+}
